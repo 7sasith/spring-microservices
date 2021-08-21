342e84b52cc4f82e5283d190a29a7513de35a82d
Sasith Weranja <7sasith@gmail.com>
2021-08-21 18:36:30 +0530

    added new files

--- user-service/src/main/java/com/example/userservice/controllers/UserController.java
+++ user-service/src/main/java/com/example/userservice/controllers/UserController.java
@@ -1,8 +1,17 @@
 package com.example.userservice.controllers;
 
+import com.example.userservice.dto.UserDTO;
+import com.example.userservice.entity.UserEntity;
 import com.example.userservice.model.CreateUserRequest;
+import com.example.userservice.model.CreateUserResponse;
+import com.example.userservice.service.UserService;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.modelmapper.spi.MatchingStrategy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -13,6 +22,8 @@ public class UserController {
 
     @Autowired
     private Environment env;
+    @Autowired
+    private UserService userService;
 
     @GetMapping("/status/check")
     public String checkStatus() {
@@ -20,7 +31,14 @@ public class UserController {
     }
 
     @PostMapping
-    public String create(@Valid @RequestBody CreateUserRequest createUser) {
-        return "sberwgerghwr";
+    public ResponseEntity<CreateUserResponse> create(@Valid @RequestBody CreateUserRequest createUser) {
+
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        UserDTO userDTO = modelMapper.map(createUser, UserDTO.class);
+        userDTO = userService.createUser(userDTO);
+
+        CreateUserResponse createUserResponse = modelMapper.map(userDTO, CreateUserResponse.class);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createUserResponse);
     }
 }
--- user-service/src/main/java/com/example/userservice/service/impl/UserServiceImpl.java
+++ user-service/src/main/java/com/example/userservice/service/impl/UserServiceImpl.java
@@ -1,11 +1,35 @@
 package com.example.userservice.service.impl;
 
 import com.example.userservice.dto.UserDTO;
+import com.example.userservice.entity.UserEntity;
+import com.example.userservice.repository.UserRepository;
 import com.example.userservice.service.UserService;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
+@Service("userService")
 public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
     @Override
     public UserDTO createUser(UserDTO useDTO) {
-        return null;
+
+        useDTO.setUserId(UUID.randomUUID().toString());
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        UserEntity userEntity = modelMapper.map(useDTO, UserEntity.class);
+        userEntity.setEncryptedPwd(bCryptPasswordEncoder.encode(useDTO.getPassword()));
+        userEntity = userRepository.save(userEntity);
+        UserDTO returnValue = modelMapper.map(userEntity, UserDTO.class);
+        return returnValue;
     }
 }
